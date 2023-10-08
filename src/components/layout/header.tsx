aa1eee81e1febe4d6c1a3f61fcd504d811ab95a7
vasyaqwe <vasyaqwe>
2023-10-08 23:28:40 +0300

    feat: make header a server component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,14 @@
-"use client"
-
 import Link from "next/link"
 import logo from "@public/logo.svg"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { AccountMenu } from "@/components/ui/account-menu"
-import { useSession } from "next-auth/react"
 import { SearchBar } from "@/components/search-bar"
+import { getAuthSession } from "@/lib/auth"
 
-export function Header() {
-    const { data: session } = useSession()
+export async function Header() {
+    const session = await getAuthSession()
+
     return (
         <header className="fixed left-0 top-0 z-10 h-[var(--header-height)] w-full border-b bg-neutral/50 backdrop-blur-md">
             <div className="container flex h-[var(--header-height)] items-center justify-between gap-4">
