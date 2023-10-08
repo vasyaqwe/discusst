caff9097c19319e34694524f82cf450b67fb31e6
vasyaqwe <vasyaqwe>
2023-10-08 13:25:30 +0300

    feat: remove logic for only showing posts from joined communities on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { PostFeed } from "@/components/post-feed"
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
 import { POSTS_INFINITE_SCROLL_COUNT } from "@/config"
-import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
 import { HomeIcon } from "lucide-react"
 import Link from "next/link"
@@ -11,61 +10,25 @@ export const dynamic = "force-dynamic"
 export const fetchCache = "force-no-store"
 
 export default async function Home() {
-    const session = await getAuthSession()
-
-    const generalPosts = session
-        ? []
-        : await db.post.findMany({
-              orderBy: {
-                  createdAt: "desc",
-              },
-              include: {
-                  votes: true,
-                  author: true,
-                  community: true,
-                  comments: true,
-              },
-              take: POSTS_INFINITE_SCROLL_COUNT,
-          })
-
-    const joinedCommunities = !session
-        ? []
-        : await db.subscription.findMany({
-              where: {
-                  userId: session?.user.id,
-              },
-          })
-
-    const customPosts = !session
-        ? []
-        : await db.post.findMany({
-              where: {
-                  communityId: {
-                      in: joinedCommunities.map((c) => c.communityId),
-                  },
-              },
-              orderBy: {
-                  createdAt: "desc",
-              },
-              include: {
-                  votes: true,
-                  author: true,
-                  community: true,
-                  comments: true,
-              },
-              take: POSTS_INFINITE_SCROLL_COUNT,
-          })
+    const posts = await db.post.findMany({
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            votes: true,
+            author: true,
+            community: true,
+            comments: true,
+        },
+        take: POSTS_INFINITE_SCROLL_COUNT,
+    })
 
     return (
         <>
             <h1 className="text-4xl font-bold">Your feed</h1>
             <div className="mt-3 grid gap-4 md:mt-6 md:grid-cols-3 lg:gap-6">
                 <div className="flex flex-col gap-5 md:col-span-2">
-                    {session ? (
-                        <PostFeed initialPosts={customPosts} />
-                    ) : (
-                        <PostFeed initialPosts={generalPosts} />
-                    )}
+                    <PostFeed initialPosts={posts} />
                 </div>
                 <Card className="self-start overflow-hidden p-0 md:p-0">
                     <div className="bg-neutral p-5 lg:p-6">
